33202fe8beff178400fef23fbc23190e81edd025
Julia Strelkova <jul.newj@gmail.com>
2026-08-05 20:38:51 +0300

    fixed dropdown in Bulk Actions

--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -63,7 +63,7 @@ function SelectContent({
   return (
     <SelectPortal>
       <SelectPrimitive.Positioner
-        className="isolate z-50 outline-none"
+        className="isolate z-66 outline-none"
         align={align}
         alignOffset={alignOffset}
         side={side}
@@ -73,7 +73,7 @@ function SelectContent({
         <SelectPrimitive.Popup
           data-slot="select-content"
           className={cn(
-            "z-50 max-h-(--available-height) w-(--anchor-width) min-w-32 origin-(--transform-origin) overflow-x-hidden overflow-y-auto rounded bg-raise p-1.5 text-popover-foreground shadow-md ring-1 ring-foreground/10 duration-100 outline-none data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 data-closed:animate-out data-closed:overflow-hidden data-closed:fade-out-0 data-closed:zoom-out-95",
+            "z-66 max-h-(--available-height) w-(--anchor-width) min-w-32 origin-(--transform-origin) overflow-x-hidden overflow-y-auto rounded bg-raise p-1.5 text-popover-foreground shadow-md ring-1 ring-foreground/10 duration-100 outline-none data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 data-closed:animate-out data-closed:overflow-hidden data-closed:fade-out-0 data-closed:zoom-out-95",
             className,
           )}
           {...props}
